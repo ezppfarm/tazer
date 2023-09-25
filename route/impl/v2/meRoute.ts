2b3d28fdb99d3e218b035875a035d27ad31c23ab
Gitea <gitea@fake.local>
2023-09-25 07:44:44 +0000

    wip user class, user repo and me route

---
+++ route/impl/v2/meRoute.ts
@@ -0,0 +1,18 @@
+import { FastifyReply, FastifyRequest } from "fastify";
+import { RequestType } from "../../requestType";
+import routeHandler from "../../routeHandler";
+
+export default class oAuthRoute implements routeHandler {
+  path = "/api/v2/me";
+  requestTypes = [RequestType.POST];
+  handle(
+    request: FastifyRequest,
+    response: FastifyReply,
+  ): unknown {
+    response.type("application/json");
+
+    const authorization_token = request.headers.authorization?.split(" ")[1];
+  
+    return {};
+  }
+}
