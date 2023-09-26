0e9f69dfb33a4325f7e75c8e718095a6935b60a2
Gitea <gitea@fake.local>
2023-09-26 08:54:11 +0000

    trim ip to prevent issues

--- utils/requestUtils.ts
+++ utils/requestUtils.ts
@@ -2,8 +2,8 @@ import { FastifyRequest } from "fastify";
 
 export const getRequestIP = (req: FastifyRequest): string => {
   if ("cf-connecting-ip" in req.headers && req.headers['cf-connecting-ip'] != undefined)
-    return req.headers['cf-connecting-ip'] as string;
+    return (req.headers['cf-connecting-ip'] as string).trim();
   if ("x-real-ip" in req.headers && req.headers['x-real-ip'] != undefined)
-    return req.headers['x-real-ip'] as string;
-  return req.ip;
+    return (req.headers['x-real-ip'] as string).trim();
+  return req.ip.trim();
 }
