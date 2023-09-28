ba92edd1bea4d54831b338f014e7e539d54d17c7
HorizonCode <rapeon3879@gmail.com>
2023-09-28 14:03:40 +0000

    type fixes

--- log/logger.ts
+++ log/logger.ts
@@ -1,20 +1,20 @@
 import ansiColors from 'ansi-colors';
 import * as glob from '../glob';
 
-export function debug(message: any) {
+export function debug(message: string) {
   const isDebug = glob.getEnv('DEVELOPER_MODE', 'false') === 'true';
   if (isDebug)
     console.log(ansiColors.bold.cyan('✎') + ' ' + ansiColors.bold(message));
 }
-export function info(message: any) {
+export function info(message: string) {
   console.log(ansiColors.bold.blue('🛈') + ' ' + ansiColors.bold(message));
 }
-export function warn(message: any) {
+export function warn(message: string) {
   console.log(ansiColors.bold.yellow('⚠') + ' ' + ansiColors.bold(message));
 }
-export function error(message: any) {
+export function error(message: string) {
   console.log(ansiColors.red.red('✖') + ' ' + ansiColors.bold(message));
 }
-export function success(message: any) {
+export function success(message: string) {
   console.log(ansiColors.green('✔') + ' ' + ansiColors.bold(message));
 }
